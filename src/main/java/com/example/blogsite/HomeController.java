2ae08a2792e5305296f6b13285028b9f032d200b
jisookwon <jinskwon@hotmail.com>
2019-05-13 08:06:54 -0400

    changed controller and many HTML files

--- src/main/java/com/example/blogsite/HomeController.java
+++ src/main/java/com/example/blogsite/HomeController.java
@@ -21,32 +21,35 @@ public class HomeController {
         return "profile";
     }
 
-
     @RequestMapping("/blog")
     public String blog() {
         return "blog";
     }
 
     @RequestMapping("/blog1")
-    public String github() {
+    public String blog1() {
         return "blog1";
+    }
+    @RequestMapping("/blog1-1")
+    public String blog11() {
+        return "blog1-1";
     }
 
     @RequestMapping("/blog2")
-    public String blog2() {
+    public String blog2(){
+
         return "blog2";
     }
-
     @RequestMapping("/blog2-1")
     public String blog21() {
+
         return "blog2-1";
     }
 
     @RequestMapping("/blog3")
     public String blog3() {
-        return "blog3";
+    return "blog3";
     }
-
     @RequestMapping("/blog3-1")
     public String blog31() {
         return "blog3-1";
@@ -56,15 +59,18 @@ public class HomeController {
     public String blog4() {
         return "blog4";
     }
-
     @RequestMapping("/blog4-1")
     public String blog41() {
         return "blog4-1";
     }
 
-    @RequestMapping("/Template")
-    public String template() {
-        return "Template";
+    @RequestMapping("/blog5")
+    public String blog5() {
+        return "blog5";
+    }
+    @RequestMapping("/blog5-1")
+    public String blog51() {
+        return "blog5-1";
     }
 
     @RequestMapping("/usinggit")
@@ -75,11 +81,6 @@ public class HomeController {
     @RequestMapping("/gitfork")
     public String gitfork() {
         return "gitfork";
-    }
-
-    @RequestMapping("/gitfork-2")
-    public String gitfork2() {
-        return "gitfork-2";
     }
 
     @RequestMapping("/introtohtml")
